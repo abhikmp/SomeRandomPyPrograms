6b179dac13e9ef2d2edb2700efa66477823a78a1
Abhijeeth S <abhikampurath@gmail.com>
2020-07-16 23:56:57 +0530

    Update and rename temp0.3_noOfWords.py to NumberOfWords.py

--- NumberOfWords.py
+++ NumberOfWords.py
@@ -1,4 +1,4 @@
-#program to count number of words in a text file
+#program to count occurence of each word and find the maximum recurring word.
 file = open("hello.txt",'r')
 newlst = dict()
 for word in file:
@@ -6,6 +6,8 @@ for word in file:
     for item in lst:
         newlst[item] = newlst.get(item,0)+1
 
+for key in newlst:
+    print(key,newlst[key])
 maxWord = max(newlst.values())
 print(list(newlst.keys())[list(newlst.values()).index(maxWord)]+":"+str(maxWord))
 
